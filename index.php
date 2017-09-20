1579aa9fe261d82bc6f418a26f6da4bd986e9aca
Harley Cheer <hcheer@csumb.edu>
2017-09-20 05:53:02 +0000

    created new workspace and generic code

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<!--Must include at least two loops (e.g., for, while, do while) 	        -->
+<!--Must include at least two conditions  (e.g., if, switch, ternary) 	    -->
+<!--Must have at least 15 CSS rules in an external file 	                -->
+<!--Must use the rand() function at least once 	                            -->
+<!--Uses at least two images 	                                            -->
+<!--Uses at least one array                                                 -->
+<!--Uses at least three array functions 	                                -->
+<!--All elements (loops, arrays, images, etc.) have a common goal           -->
+
+<?php
+
+
+include 'inc/func.php';
+
+    
+?>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+        <meta charset="utf-8" />
+        <title>
+            
+        </title>
+    </head>  
+        <body>
+            <header>
+                <h1></h1>
+            </header>
+            <? php
+            
+            
+               play();
+            ?>
+        </body>
+    <footer>
+        
+    </footer>
+</html>
